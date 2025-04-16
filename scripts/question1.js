48157b82beea2c99f409d86413a650f4c36266e8
AliMoussa-13 <ali.moussa2004.13@gmail.com>
2025-04-16 22:46:21 +0300

    creating function that calculate the average of grade

--- scripts/question1.js
+++ scripts/question1.js
@@ -7,6 +7,20 @@ class Student{
     }
 }
 
-score1=[1,2,3];
-s1=new Student("ali",score1)
-console.log(s1)
+let score1=[90,85,92];
+let score2=[70,68,72];
+let score3=[100,100,100];
+let s1=new Student("Ali",score1);
+let s2=new Student("Bob",score2);
+let s3=new Student("Charlie",score3);
+
+function calculateAverage(scoreArray){
+    let sum=0;
+    for(let i=0;i<scoreArray.length;i++){
+        sum+=scoreArray.at(i)
+    }
+    return (sum/scoreArray.length)
+}
+
+
+console.log(calculateAverage(s1.scores))
